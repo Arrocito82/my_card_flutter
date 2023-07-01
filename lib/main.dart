ff2c91dc41f4e99ad9d7760dc61ec079bf48819a
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2023-07-01 17:29:30 -0600

    Layout challenge achieved

--- lib/main.dart
+++ lib/main.dart
@@ -11,14 +11,38 @@ class MyApp extends StatelessWidget{
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            padding: EdgeInsets.all(25.0),
-            margin: EdgeInsets.symmetric(vertical: 25.0,horizontal: 25.0),
-            child: Text("Donuts"),
-            color: Colors.white70,
-          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                width: 100.0,
+                color: Colors.redAccent,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+              Container(
+                width: 100.0,
+                // padding: EdgeInsets.all(25.0),
+                // margin: EdgeInsets.symmetric(vertical: 25.0,horizontal: 25.0),
+                // child: Text("Donuts"),
+                color: Colors.blueAccent,
+              ),
+            ],
+          )
         ),
       ),
       debugShowCheckedModeBanner: false,
