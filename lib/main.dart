9f93866f0c0253fcd2db802ad682b23cd2d8eec4
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2023-07-01 17:17:03 -0600

    Upgrade JDK and Gradle version

--- lib/main.dart
+++ lib/main.dart
@@ -9,24 +9,13 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
-        // appBar: AppBar(
-        //   backgroundColor: Colors.black12,
-        //   title: Text(
-        //     "My Id Card",
-        //     style: TextStyle(
-        //       fontSize: 25,
-        //       color: Colors.white60
-        //     ),
-        //   ),
-        //   centerTitle: true,
-        // ),
-        backgroundColor: Colors.lightGreen,
+        backgroundColor: Colors.teal,
         body: SafeArea(
           child: Container(
-            height: 150.0,
-            width: 250.0,
+            height: 100.0,
+            width: 100.0,
             padding: EdgeInsets.all(25.0),
-            margin: EdgeInsets.symmetric(vertical: 100,horizontal: 50),
+            margin: EdgeInsets.symmetric(vertical: 25.0,horizontal: 25.0),
             child: Text("Donuts"),
             color: Colors.white70,
           ),
