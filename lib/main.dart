111c2a4bec170c7ee8a35775d10866fbd32a0508
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2023-07-01 19:13:34 -0600

    Try create my card challenge

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,86 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage("images/profile_picture.png"),
+              ),
+              Text(
+                "Andrea Monterrosa",
+                style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Orbitron"),
+                textAlign: TextAlign.center,
+              ),
+              Text(
+                "Flutter Developer",
+                style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500),
+                textAlign: TextAlign.center,
+              ),
               Container(
                 width: 100.0,
-                color: Colors.redAccent,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.green,
-                  ),
-                ],
+                height: 3,
               ),
               Container(
-                width: 100.0,
-                // padding: EdgeInsets.all(25.0),
-                // margin: EdgeInsets.symmetric(vertical: 25.0,horizontal: 25.0),
-                // child: Text("Donuts"),
-                color: Colors.blueAccent,
+                // margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 100.0),
+                color: Colors.white70,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(10.0),
+                      child: Image(
+                        image: AssetImage("images/icons8-phone-50.png"),
+                        fit: BoxFit.contain,
+                        width: 30.0,
+                      ),
+                    ),
+                    Text("+501 2250-555")
+                  ],
+                ),
               ),
+              Container(
+                // margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 100.0),
+                color: Colors.white70,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(10.0),
+                      child: Image(
+                        image: AssetImage("images/icons8-email-30.png"),
+                        fit: BoxFit.contain,
+                        width: 30.0,
+                      ),
+                    ),
+                    Text("moralesandrea.edr4@gmail.com")
+                  ],
+                ),
+              ),
+
+              // Spacer()
             ],
-          )
+          ),
         ),
       ),
       debugShowCheckedModeBanner: false,
