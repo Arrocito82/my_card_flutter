e339a2145500717c4fc176726545838ba8625402
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2023-07-03 10:55:06 -0600

    Card and Icon widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,7 +23,7 @@ class MyApp extends StatelessWidget {
                 "Andrea Monterrosa",
                 style: TextStyle(
                     color: Colors.white,
-                    fontSize: 40,
+                    fontSize: 35,
                     // fontWeight: FontWeight.normal,
                     fontFamily: "Pacifico"),
                 textAlign: TextAlign.center,
@@ -35,50 +34,70 @@ class MyApp extends StatelessWidget {
                     color: Colors.teal.shade100,
                     letterSpacing: 2.5,
                     fontFamily: "Orbitron",
-                    fontSize: 25,
+                    fontSize: 20,
                     fontWeight: FontWeight.w500),
                 textAlign: TextAlign.center,
               ),
-              Container(
+              SizedBox(
                 width: 100.0,
-                height: 3,
+                height: 1.0,
               ),
-              Container(
-                // margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 100.0),
-                color: Colors.white70,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(10.0),
-                      child: Image(
-                        image: AssetImage("images/icons8-phone-50.png"),
-                        fit: BoxFit.contain,
-                        width: 30.0,
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    // mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.phone_iphone,
                         color: Colors.teal,
+                        size: 20.0,
                       ),
-                    ),
-                    Text("+501 2250-555")
-                  ],
+                      SizedBox(width: 10.0),
+                      Text(
+                        "+501 2250-555",
+                        style: TextStyle(
+                            fontFamily: "Orbitron",
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14.0,
+                            color: Colors.teal,
+                            letterSpacing: 1.5),
+                      )
+                    ],
+                  ),
                 ),
               ),
-              Container(
-                // margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 100.0),
-                color: Colors.white70,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      padding: EdgeInsets.all(10.0),
-                      child: Image(
-                        image: AssetImage("images/icons8-email-30.png"),
-                        fit: BoxFit.contain,
-                        width: 30.0,
+              Card(
+                // padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                color: Colors.white,
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    // mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.email_rounded,
                         color: Colors.teal,
+                        size: 20.0,
                       ),
-                    ),
-                    Text("moralesandrea.edr4@gmail.com")
-                  ],
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        "moralesandrea.edr4@gmail.com",
+                        style: TextStyle(
+                          fontFamily: "Orbitron",
+                          fontWeight: FontWeight.w700,
+                          fontSize: 14.0,
+                          color: Colors.teal,
+                          letterSpacing: 1.5,
+                        ),
+                      )
+                    ],
+                  ),
                 ),
               ),
 
